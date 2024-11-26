e04883d6ad39ef772d559b41e4912458480e9f7d
Disira Chanthuka <disira.chanthuka@codimite.com>
2024-11-26 07:28:50 +0530

    fix: re implementing the view for the popup

--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { IconSun, IconMoonStars } from '@tabler/icons-react'
+import { BsMoonStars, BsGlobe, BsFillMoonStarsFill } from 'react-icons/bs'
+import { IconContext } from 'react-icons'
 import {
   useComputedColorScheme,
   useMantineColorScheme,
@@ -9,12 +10,14 @@ import {
   Text,
   Group,
   Card,
+  ThemeIcon,
+  Paper,
+  Image,
 } from '@mantine/core'
 import classes from './Popup.module.css'
 
 export const Popup = () => {
   const [currentDomain, setCurrentDomain] = useState('')
-
   const { colorScheme, setColorScheme } = useMantineColorScheme()
   const computedColorScheme = useComputedColorScheme('light')
 
@@ -32,15 +35,34 @@ export const Popup = () => {
     })
   }, [])
 
+  useEffect(() => {
+    chrome.storage.sync.get(['colorScheme']).then((res) => {
+      if (res.colorScheme) {
+        setColorScheme(res.colorScheme)
+        console.log(`colorscheme set to ${res.colorScheme} when mounting`)
+      }
+    })
+  }, [])
+
+  //toggling options for user
   const toggleColorScheme = () => {
     console.log('dark mode fired')
-    setColorScheme(computedColorScheme === 'dark' ? 'light' : 'dark')
+    const updatedColorScheme = computedColorScheme === 'dark' ? 'light' : 'dark'
+    setColorScheme(updatedColorScheme)
+    chrome.storage.sync.set({ colorScheme: updatedColorScheme }).then(() => {
+      console.log(`setting into chrome storage sync colorScheme ${updatedColorScheme}`)
+    })
+    console.log('updated color scheme: ', updatedColorScheme)
   }
 
   const toggleForWebsite = () => {
     console.log('website toggle clicked')
+    chrome.storage.sync.get(['colorScheme']).then((res) => {
+      console.log(`value from storage is ${res.colorScheme}`)
+    })
   }
 
+  //toggling content
   const optionsForUser = [
     {
       title: 'Dark Mode',
@@ -54,6 +76,7 @@ export const Popup = () => {
     },
   ]
 
+  //user options items
   const items = optionsForUser.map((item) => (
     <div className={classes.itemWrapper} key={item.title}>
       <Group justify="space-between" className={classes.item} wrap="nowrap" gap="xl">
@@ -93,15 +116,90 @@ export const Popup = () => {
   //   }, []);
 
   return (
-    <Card withBorder radius="md" p="xl" className={classes.card}>
-      <Text fz="lg" className={classes.title} fw={500}>
-        Intellawrite
-      </Text>
-      <Text fz="xs" c="dimmed" mt={3} mb="xl">
-        Testing desc
-      </Text>
-      {items}
-    </Card>
+    <>
+      <Card withBorder radius="md" p="md" className={classes.card}>
+        <div className={classes.itemWrapper}>
+          <Group justify="space-between" className={classes.item} wrap="nowrap" gap="xl">
+            <div>
+              <Text fw="500">Get Started</Text>
+            </div>
+          </Group>
+        </div>
+
+        <div className={classes.itemWrapper}>
+          <Group justify="space-between" className={classes.item} wrap="nowrap" gap="xl">
+            <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+              <IconContext.Provider value={{ color: 'purple' }}>
+                {colorScheme === 'light' ? (
+                  <BsMoonStars size={20} />
+                ) : (
+                  <BsFillMoonStarsFill size={20} />
+                )}
+              </IconContext.Provider>
+              <div>
+                <Text fw="500"> Dark Mode</Text>
+                <Text size="xs" c="dimmed">
+                  Enjoy a sleek and comfotable experience
+                </Text>
+              </div>
+            </div>
+            {colorScheme === 'light' ? (
+              <ThemeIcon
+                variant="gradient"
+                size="lg"
+                aria-label="Gradient action icon"
+                gradient={{ from: 'violet', to: 'cyan', deg: 90 }}
+              >
+                <BsMoonStars size={20} />
+              </ThemeIcon>
+            ) : (
+              <ThemeIcon
+                variant="gradient"
+                size="lg"
+                aria-label="Gradient action icon"
+                gradient={{ from: 'grey', to: 'black', deg: 90 }}
+              >
+                <BsFillMoonStarsFill size={20} />
+              </ThemeIcon>
+            )}
+
+            <Switch
+              onLabel="ON"
+              offLabel="OFF"
+              className={classes.switch}
+              size="md"
+              color="grape"
+              onClick={toggleColorScheme}
+              checked={colorScheme === 'dark'}
+            />
+          </Group>
+        </div>
+
+        <div className={classes.itemWrapper}>
+          <Group justify="space-between" className={classes.item} wrap="nowrap" gap="xl">
+            <div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+              <IconContext.Provider value={{ color: 'purple' }}>
+                <BsGlobe size={20} />
+              </IconContext.Provider>
+              <div>
+                <Text fw="500">This Website</Text>
+                <Text size="xs" c="dimmed">
+                  {currentDomain}
+                </Text>
+              </div>
+            </div>
+            <Switch
+              onLabel="ON"
+              offLabel="OFF"
+              className={classes.switch}
+              size="md"
+              color="grape"
+              onClick={toggleForWebsite}
+            />
+          </Group>
+        </div>
+      </Card>
+    </>
   )
 }
 
